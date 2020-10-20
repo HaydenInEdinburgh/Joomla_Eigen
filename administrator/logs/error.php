edb4d2f35c8da291b483887244a5bb283305d887
Pengyi Ye <pye3@wpi.edu>
2020-10-20 16:31:02 -0400

    updated

---
+++ administrator/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2020-10-20 19:39:10 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2020-10-20T19:39:10+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-10-20T19:39:17+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
